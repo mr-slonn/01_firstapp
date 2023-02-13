6f3bd1675c3d120ebb0c7269242fcc3cdeb59fa0
Mr.Slonn NetoLogy <mr.slonn@gmail.com>
2023-02-13 23:27:41 +0300

    Домашнее задание к занятию «2.1. Обработка событий в Android

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,11 +1,90 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import java.text.DecimalFormat
+import kotlin.math.floor
+import kotlin.math.log10
+import kotlin.math.pow
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false
+        )
+
+        with(binding) {
+            author.text  = post.author
+            published.text = post.published
+            content.text = post.content
+            if (post.likedByMe) {
+                like.setImageResource(R.drawable.ic_baseline_favorite_24)
+            }
+            likeCount.text = post.likes.toString()
+
+            root.setOnClickListener {
+                Log.d("stuff", "stuff")
+            }
+
+            avatar.setOnClickListener {
+                Log.d("stuff", "avatar")
+            }
+
+            share.setOnClickListener {
+
+                shared.text =  prettyCount(1000000)
+            }
+
+            like.setOnClickListener {
+                Log.d("stuff", "like")
+                post.likedByMe = !post.likedByMe
+                like.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
+                )
+                if (post.likedByMe) post.likes++ else post.likes--
+                likeCount.text = post.likes.toString()
+            }
+        }
     }
+
+    private fun toFormatViews(value:Int):String
+    {
+        if (value >= 1000000) {
+            return (value / 1000000).toString() + "M"
+        }
+
+        if (value >= 10000) {
+            return (value / 1000).toString() + "k"
+        }
+
+
+        return value.toString()
+    }
+
+    private fun prettyCount(number: Number): String? {
+        val suffix = charArrayOf(' ', 'k', 'M', 'B', 'T', 'P', 'E')
+        val numValue = number.toLong()
+        val value = floor(log10(numValue.toDouble())).toInt()
+        val base = value / 3
+        return if (value >= 3 && base < suffix.size) {
+            DecimalFormat("#0.0").format(
+                numValue / 10.0.pow((base * 3).toDouble())
+            ) + suffix[base]
+        } else {
+            DecimalFormat("#,##0").format(numValue)
+        }
+    }
+
 }
