3c10e8d1dc69b38b238558931f0d577aac8eb6b0
192.168.0.21 <antonov_vv@spbexp.ru>
2023-02-14 15:44:21 +0300

    2.2. Архитектура: MVVM

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,10 +1,12 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
-import android.util.Log
+
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.Post
+
+import ru.netology.nmedia.viewmodel.PostViewModel
 import java.math.RoundingMode
 import java.text.DecimalFormat
 import kotlin.math.ln
@@ -17,48 +19,29 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "21 мая в 18:36",
-             likes = 999
-        )
-
-        with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            shared.text = countWithSuffix(post.shared)
-            viewsCount.text = countWithSuffix(post.viewsCount)
-            if (post.likedByMe) {
-                like.setImageResource(R.drawable.ic_baseline_favorite_24)
-            }
-            likeCount.text = post.likes.toString()
-
-            root.setOnClickListener {
-                Log.d("stuff", "stuff")
-            }
-
-            avatar.setOnClickListener {
-                Log.d("stuff", "avatar")
-            }
-
-            share.setOnClickListener {
-                post.shared += 100
+        val viewModel: PostViewModel by viewModels<PostViewModel>()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
                 shared.text = countWithSuffix(post.shared)
-            }
-
-            like.setOnClickListener {
-                Log.d("stuff", "like")
-                post.likedByMe = !post.likedByMe
+                viewsCount.text = countWithSuffix(post.viewsCount)
                 like.setImageResource(
                     if (post.likedByMe) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
                 )
-                if (post.likedByMe) post.likes++ else post.likes--
-                likeCount.text =  countWithSuffix (post.likes)
+                likeCount.text = post.likes.toString()
             }
         }
+
+        binding.share.setOnClickListener {
+            viewModel.shared((viewModel.data.value?.shared ?: 0) + 100)
+        }
+
+        binding.like.setOnClickListener {
+            viewModel.like()
+        }
+
     }
 
     private fun countWithSuffix(count: Int): String {
