9aaee94907e323bf6ec99d14058d4da51487a49f
192.168.0.21 <antonov_vv@spbexp.ru>
2023-08-16 11:09:33 +0300

    1.3. Architecture Components. Часть 2

--- app/src/main/java/ru/netology/nmedia/repository/PostRemoteMediator.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRemoteMediator.kt
@@ -11,7 +11,7 @@ import ru.netology.nmedia.dao.PostRemoteKeyDao
 import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.entity.PostEntity
 import ru.netology.nmedia.entity.PostRemoteKeyEntity
-import ru.netology.nmedia.entity.toEntity
+//import ru.netology.nmedia.entity.toEntity
 import ru.netology.nmedia.error.ApiError
 
 @OptIn(ExperimentalPagingApi::class)
@@ -104,7 +104,8 @@ class PostRemoteMediator(
                         )
                     }
                 }
-                postDao.insert(body.toEntity())
+                //postDao.insert(body.toEntity())
+                postDao.insert(body.map(PostEntity::fromDto))
             }
 
 
